3a4430a86be200a95f31ed8ea429a71b5a6f84d0
Berk Bacalan <berkbacalan@gmail.com>
2022-07-03 18:19:52 +0300

    create url schemas

---
+++ app/schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class URLBase(BaseModel):
+    original_url: str
+
+class URL(URLBase):
+    clicks: int
+
+    class Config:
+        orm_mode = True
+
+class URLInfo(URL):
+    short_url: str
