37bc5e78c6b32aa62c607cc88233f702c35a5efe
Berk Bacalan <berkbacalan@gmail.com>
2022-07-03 18:57:47 +0300

    add create and list api's

--- app/main.py
+++ app/main.py
@@ -1,7 +1,62 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi.responses import RedirectResponse
+import string
+import random
+
+from sqlalchemy.orm import Session
+
+from . import models
+from .schema import URLInfo, URL, URLBase, URLListResponse
+from .database import SessionLocal, engine
+
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
     return "Default backend with status 200"
+
+
+@app.post("/url", response_model=URLInfo)
+def create_url(url: URLBase, db: Session = Depends(get_db)):
+
+    short_url = ''.join(random.choice(string.ascii_lowercase) for i in range(6))
+    db_url = models.URL(
+        original_url=url.original_url, short_url=short_url
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+
+    return db_url
+
+
+@app.get("/list", response_model=URLListResponse)
+def get_urls(db: Session = Depends(get_db)):
+    urls = db.query(models.URL).all()
+
+    return urls
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        db: Session = Depends(get_db)
+    ):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.short_url == url_key)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.original_url)
+    else:
+        HTTPException(status_code=404, detail="No url with this key.")
--- app/models.py
+++ app/models.py
@@ -6,6 +6,6 @@ class URL(Base):
     __tablename__ = "urls"
 
     id = Column(Integer, primary_key=True)
-    target_url = Column(String, index=True)
+    original_url = Column(String, index=True)
     short_url = Column(String, index=True)
     clicks = Column(Integer, default=0)
